48066f890202d4f46f7810aaebc8c09f7106098c
Martin Ramsin <martin.ramsin@gmail.com>
2013-05-07 09:17:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lugogram::Application.config.secret_token = 'fe7e94db3431b49c57203424870d9184eda90459352aaf6f69123a48632d1aab79a2b405f3a156107933ba0536f88744d2399fc298db3b97d9054743bcb7daff'
