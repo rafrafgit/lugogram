addd3a25567e1ebfaec8d58568109d1c3bb5e550
Martin Ramsin <martin.ramsin@gmail.com>
2013-05-12 14:29:55 +0200

    updated mail settings for master

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,21 @@ Lugogram::Application.configure do
   # and use secure cookies.
   config.force_ssl = true
 
+  #config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => '',
+    :user_name            => 'martin.ramsin@gmail.com',
+    :password             => 'Godmorgon',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
