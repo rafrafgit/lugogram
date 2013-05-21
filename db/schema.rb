9117ca8894be0fb85475ef8bdba5f14bbd269ca5
Martin Ramsin <martin.ramsin@gmail.com>
2013-05-21 21:05:36 +0200

    admin users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518213000) do
+ActiveRecord::Schema.define(:version => 20130521185616) do
 
   create_table "eyes", :force => true do |t|
     t.integer  "micropost_id"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20130518213000) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.string   "avatar"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
---
+++ lib/tasks/sample_data.rake
@@ -0,0 +1,12 @@
+namespace :db do
+  desc "Fill database with sample data"
+  task populate: :environment do
+  admin = User.create!(name: "Staff",
+                         email: "lugogram@gmail.com",
+                         avatar: "https://www.lugogram.com/images/ninja-avatar-48x48.png",
+                         password: "bajen01",
+                         password_confirmation: "bajen01")
+    admin.toggle!(:admin)
+    
+  end
+end
